b82f1d475048f6dae620d73f13faa9e99a2abb9d
v-danh <vdanhvt2000@gmail.com>
2022-05-18 12:28:48 +0700

    first changes

---
+++ __init__.py
@@ -0,0 +1,6 @@
+from pip import main
+import debugger
+
+if __name__ == '__main__':
+    debugger.main()
+    debugger.__name__
---
+++ debugger.py
@@ -0,0 +1,5 @@
+def main():
+    print('ML detector and debugger for IDE using Manchine Learing')
+
+if __name__ == '__main__':
+    main()
